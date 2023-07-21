9984cdce69ce97b361ce15173b0b0a1e62dff6b7
nachinolkar <66353133+nachinolkar@users.noreply.github.com>
2023-07-21 09:30:39 +0530

    run.sh
    added -vaP for rsync

--- __obsidian/3D Printing/Cool Filaments.md
+++
@@ -1,1 +0,0 @@
-- [Silk Metallic PETG](https://www.herz-filament.at/en/44-petg)
--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ pip install python-slugify
 echo "netlify.toml" >>__obsidian/.gitignore
 
 # Sync Zola template contents
-rsync -a __site/zola/ __site/build
-rsync -a __site/content/ __site/build/content
+rsync -vaP __site/zola/ __site/build
+rsync -vaP __site/content/ __site/build/content
 
 # Use obsidian-export to export markdown content from obsidian
 mkdir -p __site/build/content/docs __site/build/__docs
