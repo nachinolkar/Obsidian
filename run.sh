8a54d114a92dd26f45cba9074a2f3b28eff4d7ea
nachinolkar <66353133+nachinolkar@users.noreply.github.com>
2023-07-22 19:09:25 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ echo "netlify.toml" >>__obsidian/.gitignore
 
 # Remove previous build and sync Zola template contents
 rm -rf build
-rsync -vaP __site/zola/ __site/build
-rsync -vaP __site/content/ __site/build/content
+#rsync -vaP __site/zola/ __site/build
+#rsync -vaP __site/content/ __site/build/content
 
 # Use obsidian-export to export markdown content from obsidian
 mkdir -p __site/build/content/docs __site/build/__docs
